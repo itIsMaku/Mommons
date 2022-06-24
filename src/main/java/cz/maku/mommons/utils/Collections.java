30ec51ec96ffdff5a867d0cef45fbf07674422ba
Adam Volkman (maku) <68100107+itIsMaku@users.noreply.github.com>
2022-06-24 10:01:49 +0200

    feat(collections): sort by value

--- src/main/java/cz/maku/mommons/utils/Collections.java
+++ src/main/java/cz/maku/mommons/utils/Collections.java
@@ -6,6 +6,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public final class Collections {
 
@@ -20,5 +22,13 @@ public final class Collections {
         throw new IllegalArgumentException("Can't find random value.");
     }
 
-
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
 }
